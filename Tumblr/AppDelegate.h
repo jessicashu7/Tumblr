08bea8aee606e452c01ad7e5dc9a445450721483
Jessica Shu <jessicashu7@fb.com>
2018-06-27 23:25:06 -0700

    Initial Commit

---
+++ Tumblr/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tumblr
+//
+//  Created by Jessica Shu on 6/27/18.
+//  Copyright © 2018 Jessica Shu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tumblr/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tumblr
+//
+//  Created by Jessica Shu on 6/27/18.
+//  Copyright © 2018 Jessica Shu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
