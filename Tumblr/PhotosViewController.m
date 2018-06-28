09daec21c7114d6046fc5c598e17f984e4e0fe72
Jessica Shu <jessicashu7@fb.com>
2018-06-28 00:05:10 -0700

    hook up to tumblr API

--- Tumblr/PhotosViewController.m
+++ Tumblr/PhotosViewController.m
@@ -10,6 +10,8 @@
 
 @interface PhotosViewController ()
 
+@property (strong, nonatomic) NSArray *posts;
+
 @end
 
 @implementation PhotosViewController
@@ -17,6 +19,25 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    NSURL *url = [NSURL URLWithString:@"https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        if (error != nil) {
+            NSLog(@"%@", [error localizedDescription]);
+        }
+        else {
+            NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+            
+            NSLog(@"%@", dataDictionary);
+            NSDictionary *responseDictionary = dataDictionary[@"response"];
+            self.posts = responseDictionary[@"posts"];
+            // TODO: Get the posts and store in posts property
+            // TODO: Reload the table view
+        }
+    }];
+    [task resume];
 }
 
 - (void)didReceiveMemoryWarning {
